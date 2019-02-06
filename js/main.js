eb5328b581adcc28cbb11075af24388ffdf43034
Jiyoung Oh <jo289@cornell.edu>
2019-02-06 23:49:43 +0900

    in progress

---
+++ js/main.js
@@ -0,0 +1,142 @@
+var margin = {top: 20, right: 10, bottom: 20, left: 10};
+var width = 960 - margin.left - margin.right;
+var height = 500 - margin.top - margin.bottom;
+var padding = 50;
+
+
+var svg = d3.select("#chart-area")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+
+var g=svg.append("g")
+    .attr("transform", "translate(0,-30)");
+
+//xAxis label
+g.append("text")
+  .attr("x",width/2)
+  .attr("y",height+margin.bottom)
+  .attr("text-anchor", "middle")
+  .attr("font-size", "20px")
+  .text("Share price change(%)");
+
+  //X Scale
+  var x = d3.scaleLinear()
+    //.domain([d3.min(data,function(d){return d.changes}),
+    //         d3.max(data,function(d){return d.changes})])
+      .domain([-80,100])
+      .range([0+padding,width-padding]);//add padding so the circle does not get cutoff
+
+
+//load data
+d3.csv("https://raw.githubusercontent.com/summerohoh/FTgraphics/master/kospi200_price_changes.csv")
+  .then(function(data){
+
+  // data.forEach(function(d){
+  //   d.code = d["Code"];
+  //   d.name = d["Issue Name"];
+  //   d.marketcap= parseFloat(d["Market Cap(KRW)"].replace(/,/g,''));
+  //   d.capweight = +d["Index Market Cap weight(%)"];
+  //   d.changes = +d["share price change"];
+  //   d.sector = "Technology";
+  // })
+
+  var nodes = data.map(function(node, index) {
+    return {
+      index: index,
+      code: node["Code"],
+      marketcap: parseFloat(node["Market Cap(KRW)"].replace(/,/g,'')),
+      capweight : +node["Index Market Cap weight(%)"],
+      changes : +node["share price change"],
+      sector : "Technology",
+      x: x(parseFloat(node["share price change"].replace(/,/g,''))),
+      fx: x(parseFloat(node["share price change"].replace(/,/g,'')))
+    };
+  });
+
+  console.log(nodes);
+
+
+  //var min = d3.min(data,function(d){return d.changes});
+
+  var circleSize =d3.scaleSqrt()
+    .domain([0, d3.max(nodes,function(d){
+    return d.marketcap})])
+    .range([0, 30])
+
+console.log(circleSize(3525916824000));
+
+  var color = d3.scaleOrdinal()
+    .domain(data.map(function(d){return d.sector}))
+    .range(d3.schemeSet3);
+
+  //add xAxis generator
+  var xAxis = d3.axisBottom(x)
+    .ticks(10)
+    .tickSize(-height)
+    .tickFormat(function(d){
+      return d + "%";
+    });
+
+  g.append("g")
+    .attr("class","x-axis")
+    .attr("transform","translate(0,450)")
+    .call(xAxis)
+
+    var simulation = d3.forceSimulation(nodes)
+      .force("y", d3.forceY(250))
+      //.force("collide", d3.forceCollide().radius(function(d){ circleSize(d.marketcap) + 0 }))
+      .force("manyBody", d3.forceManyBody().strength(-80))
+      .stop();
+
+    for (var i = 0; i < 200; ++i) simulation.tick();
+
+    var circle = svg.selectAll("circle")
+      .data(nodes)
+      .enter().append("circle")
+      //.style("fill", function(d) { return colorScale(d.value); })
+      .attr("cx", function(d) { return d.x} )
+      .attr("cy", function(d) { return d.y} )
+      .attr("r", function(d) { return circleSize(d.marketcap)} )
+      .style("opacity", 0.6);
+
+
+//   var simulation = d3.forceSimulation(nodes)
+//      .force("y", d3.forceY(height/2))
+//      .force("collide", d3.forceCollide().radius(function(d){ return circleSize(d.marketcap) + padding }))
+//      .force("manyBody", d3.forceManyBody().strength(0))
+//      .stop();
+//
+// for (var i = 0; i < 150; ++i) simulation.tick();
+//
+// var circle = svg.selectAll("circle")
+//     .data(nodes)
+//     .enter().append("circle")
+//     //.style("fill", function(d) { return colorScale(d.value); })
+//     .attr("cx", function(d,i){
+//             return x(d.changes);
+//     })
+//     .attr("cy", function(d) { return d.y} )
+//     .attr("r", function(d) { return circleSize(d.marketcap)} )
+//     .style("opacity", 0.6);
+
+
+  //plot circles
+    // var circles = g.selectAll("circle")
+    //   .data(nodes);
+    //
+    // circles.enter()
+    //   .append("circle")
+    //     .attr("cx", function(d,i){
+    //         return x(d.changes);
+    //     })
+    //     .attr("cy", height/2)
+    //     .attr ("r", function(d){
+    //       return circleSize(d.marketcap)
+    //     })
+    //     .attr("fill", "red");
+
+
+}).catch(function(error){
+  console.log(error);
+})
