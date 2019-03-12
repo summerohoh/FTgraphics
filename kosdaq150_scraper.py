4c52d331b42e98bf7c8155ccfddc80a8a2f24bef
Jiyoung Oh <jo289@cornell.edu>
2019-03-12 20:59:48 +0900

    headless chrome searches KRX

--- kosdaq150_scraper.py
+++ kosdaq150_scraper.py
@@ -7,8 +7,11 @@ import csv
 from multiprocessing import Pool
 from handlers import load_file,extract_adj_price,epoch_converter
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 
 os.chdir("/Users/summer/Desktop/FTgraphics")
@@ -20,26 +23,86 @@ kosdaq150 = kosdaq150.loc[0:3,:]
 url = "http://marketdata.krx.co.kr/contents/MKD/04/0402/04020100/MKD04020100T3T2.jsp"
 
 options = Options()
-#options.add_argument("--headless")
+options.add_argument("--headless")
 chromedriver = "/Users/summer/Desktop/FTgraphics/chromedriver"
-driver = webdriver.Chrome(chromedriver)
-#driver = webdriver.Chrome(chrome_options=options, executable_path='/chromedriver.exe')
-driver.implicitly_wait(30)
-driver.get(url)
+#driver = webdriver.Chrome(chromedriver)
 
-inputStock = driver.find_element_by_class_name("func-finder-input ")
-inputStock.clear()
-inputStock.send_keys("A005930")
-inputFromDate= driver.find_element_by_name("fromdate")
-inputFromDate.clear()
-inputFromDate.send_keys("20181228")
-inputToDate = driver.find_element_by_name("todate")
-inputToDate.clear()
-inputToDate.send_keys("20181228")
+def extract_krx_price(url):
+    driver = webdriver.Chrome(chrome_options=options, executable_path=chromedriver)
+    driver.implicitly_wait(50)
+    driver.get(url)
 
-test = driver.find_element_by_id('btnidc4ca4238a0b923820dcc509a6f75849b')
-#test = driver.find_element_by_xpath('//button[@class="btn-board-search"]')
-test.send_keys(Keys.ENTER);
+    price = None
+
+    try:
+
+        inputStock = driver.find_element_by_class_name("func-finder-input ")
+        inputStock.clear()
+        inputStock.send_keys("A005930")
+        inputFromDate= driver.find_element_by_name("fromdate")
+        inputFromDate.clear()
+        inputFromDate.send_keys("20181228")
+        inputToDate = driver.find_element_by_name("todate")
+        inputToDate.clear()
+        inputToDate.send_keys("20181228")
+        inputToDate.send_keys(Keys.TAB)
+
+        element = WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.ID, "btnidc4ca4238a0b923820dcc509a6f75849b")))
+
+        element.click()
+        req = driver.page_source
+        soup=BeautifulSoup(req, 'html.parser')
+
+        info_row = soup.find("tbody", {"class":"CI-GRID-BODY-TABLE-TBODY"}).find_all("tr")[0]
+        adj_price = info_row.find("td", {"data-name":"tdd_clsprc"}).text.replace(",","")
+        price = adj_price
+
+    finally:
+        driver.quit()
+
+    return (price)
+
+
+test = extract_krx_price(url)
+print(test)
+#
+#
+# driver = webdriver.Chrome(chrome_options=options, executable_path=chromedriver)
+# driver.implicitly_wait(50)
+# driver.get(url)
+#
+# try:
+#
+#     inputStock = driver.find_element_by_class_name("func-finder-input ")
+#     inputStock.clear()
+#     inputStock.send_keys("A005930")
+#     inputFromDate= driver.find_element_by_name("fromdate")
+#     inputFromDate.clear()
+#     inputFromDate.send_keys("20181228")
+#     inputToDate = driver.find_element_by_name("todate")
+#     inputToDate.clear()
+#     inputToDate.send_keys("20181228")
+#     inputToDate.send_keys(Keys.TAB)
+#
+#
+#     element = WebDriverWait(driver, 20).until(
+#     EC.element_to_be_clickable((By.ID, "btnidc4ca4238a0b923820dcc509a6f75849b")))
+#
+#     element.click()
+#     req = driver.page_source
+#     soup=BeautifulSoup(req, 'html.parser')
+#
+#     info_row = soup.find("tbody", {"class":"CI-GRID-BODY-TABLE-TBODY"}).find_all("tr")[0]
+#     adj_price = info_row.find("td", {"data-name":"tdd_clsprc"}).text.replace(",","")
+#     print(adj_price)
+#
+# finally:
+#     driver.quit()
+#
+# #
+#
+# driver.find_element_by_id("btnidd9d4f495e875a2e075a1a4a6e1b9770f").send_keys(Keys.RETURN)
 
 
 # def get_url():
